51d00d54862216fe93a13d9f5a9a65aef557cc06
ArdataCode <ardata.indonesia@gmail.com>
2024-05-19 09:43:38 +0000

    coa

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/u726706882/domains/staialhikmahtuban.ac.id/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/u726706882/domains/staialhikmahtuban.ac.id/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", 'df3bde554a24f0b74d07f3670cbfd656');
+	include_once('/home/u726706882/domains/staialhikmahtuban.ac.id/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'hnEXzHz3UnY4Up4MMV87H1o9JWYEyVQeR7Xv5pmHTE9rrnAAyzMMQqW3hV7GKnDA');
+define('SECURE_AUTH_KEY',  'RdcE1IRcpFoUGKn31YmHw0oQwRhYu4wjjhjCn55maL19QWRjBT3zPI72sY1QSs3q');
+define('LOGGED_IN_KEY',    'atrBsXNREFIScFWPxtwJi4sxCvA4WyU5YexuAevKHvvVeKN3hFDQHrKqcFBFu7Im');
+define('NONCE_KEY',        'WPraCGfmL0bI0n5doCWYQqNToMfxG0KdKv1dbF7nL8QGtMFdgjbCEMM635yHWwcp');
+define('AUTH_SALT',        'upVTdDa0TroLPyFc0eFybYfUzIRzLwpYvzeqFQvT2Ljxqdqgrz4aLsWxc96DJuXG');
+define('SECURE_AUTH_SALT', 'XB9wbiYpRQHGwy82eaRYEtudaYVAaWLwzvweVFrzppaCjrcaBf21NAPcfohc7Tdd');
+define('LOGGED_IN_SALT',   '7N9CRS4Dgax7TM6iEDegVIEH3GbL2ga5gF87D4m1pQM5qpBT5dQUJpwLYunGXogr');
+define('NONCE_SALT',       'KIMVwBvC6W9NXpNqL15SeyIgDuXgvK3D66Lt3UnUsAq19yyA93APf65i43T0DRQs');
